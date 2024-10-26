2a3460b6e1e80fc23c33c8d612e9a9a3eb4db304
cs-imms <maitre.imms@gmail.com>
2024-10-26 14:21:22 +0100

    fix bug for .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MonitorModule } from './monitor/monitor.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MonitorModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Rendre les variables d'environnement accessibles globalement
+    }),
+    MonitorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
