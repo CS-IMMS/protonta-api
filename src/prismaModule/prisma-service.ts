14d4b55170eeb5265debbf946856095b257654c3
cs-imms <maitre.imms@gmail.com>
2024-10-26 16:23:47 +0100

    remove prisma

--- src/prismaModule/prisma-service.ts
+++ src/prismaModule/prisma-service.ts
@@ -1,17 +1,17 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-@Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    console.log('database initialized successfully');
-    await this.$connect();
-  }
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   async onModuleInit() {
+//     console.log('database initialized successfully');
+//     await this.$connect();
+//   }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-}
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
