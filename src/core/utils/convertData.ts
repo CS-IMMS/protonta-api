9317a7a788297fcf096629ca3a3a60a87dd612f3
Ismael Mahamadou Moussa <ismael.mahamadou_moussa@bennu.tech>
2024-12-10 13:56:44 +0000

    Refactor parseSensorDataMonitor to improve pollination period calculation. Updated PeriodePol to use a more accurate conversion from milliseconds to minutes, enhancing data consistency in sensor data parsing.

--- src/core/utils/convertData.ts
+++ src/core/utils/convertData.ts
@@ -91,7 +91,7 @@ const parseSensorDataMonitor = (dataString: string) => {
     pollinationEndTime: String(
       convertMillisecondsToHours(Number(dataArray[44])),
     ),
-    PeriodePol: String(convertMillisecondsToHours(Number(dataArray[45]))),
+    PeriodePol: String(Math.floor(Number(dataArray[45])) / (60 * 1000)),
     ManuelAutoS1: Number(dataArray[46]),
     ManuelAutoS2: Number(dataArray[47]),
     ManuelAutoS3: Number(dataArray[48]),
